41cb0b52d533689e8a32d6ead65a28ef10c0588b
Edna Wangui <your-email@example.com>
2025-09-07 07:54:54 +0300

    Fix query_books_by_author and query_librarian_by_library

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -5,8 +5,9 @@ def query_books_by_author(author_name):
     return Book.objects.filter(author__name=author_name)
 
 # List all books in a library
-def query_books_in_library(library_name):
-    return Book.objects.filter(library__name=library_name)
+def list_books_in_library(library_name):
+    library = Library.objects.get(name=library_name)
+    return library.books.all()
 
 # Retrieve the librarian for a library
 def query_librarian_by_library(library_name):
